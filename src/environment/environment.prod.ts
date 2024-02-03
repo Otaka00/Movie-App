f41eecb0798ac7e03747cc0251f289d247605fea
otaka00 <ahmad_otaca@hotmail.com>
2024-02-03 16:17:17 +0200

    Edit environment

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.themoviedb.org/3',
   apiKey: 'your_production_api_key',
-  imageBaseUrl: 'https://image.tmdb.org/t/p/original'
+  imageBaseUrl: 'https://image.tmdb.org/t/p/original',
+    backendApi:'http://localhost:8081/api/v1/movies'
+
 };
